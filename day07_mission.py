c921349d832298bf085700527382d7f7b328cff2
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-20 20:07:49 +0900

    10.8

--- day07_mission.py
+++ day07_mission.py
@@ -1,20 +1,30 @@
-#10.7
-
+#10.8
 
 
 el_dict = {'name': 'Hydrogen', 'symbol': 'H', 'number': 1}
 
 class Element:
     def __init__(self, name, symbol, number):
-        self.name = name
-        self.symbol = symbol
-        self.number = number
-    def __str__(self):
-        return(f'{self.name},안녕 {self.symbol}, {self.number}')
+        self.__name = name
+        self.__symbol = symbol
+        self.__number = number
+        @property
+        def names(self):
+            return (self.__name, self.__symbol, self.__number)
+        @name.setter
+        def name2(self, name):
+            self.__name = name
+            # self.__symbol = symbol
+            # self.__number = number
+
+    # def __str__(self):
+    #     return(f'{self.name},안녕 {self.symbol}, {self.number}')
 
 
-hydrogen = Element(**el_dict)
+hydrogen = Element("Hydden","H",1)
+hydrogen.names()
 print(hydrogen)
+
 
 
 
