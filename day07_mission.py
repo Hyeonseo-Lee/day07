6ba7f1cab5c307387d2e30e4488bba72ecd8415b
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-20 18:53:02 +0900

    10.5

--- day07_mission.py
+++ day07_mission.py
@@ -1,10 +1,12 @@
-#10.4
+#10.5
+el_dict = {'name': 'Hydrogen', 'symbol': 'H', 'number': 1}
 
 class Element:
-    def __init__(self, name, symbol, number):
-        self.name = name
-        self.symbol = symbol
-        self.number = number
+    def __init__(self, eldict):
+        self.name = eldict['name']
+        self.symbol = eldict['symbol']
+        self.number = eldict['number']
 
-ele = Element('Hydrogen', 'H', 1)
-print(ele.symbol)
+hydrogen = Element(el_dict)
+print(hydrogen.name)
+
