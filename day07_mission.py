fb84102186d229be64c9fd2eeac6cb2125fde87f
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-20 20:22:35 +0900

    10.9

--- day07_mission.py
+++ day07_mission.py
@@ -1,29 +1,29 @@
-#10.8
+
+#10.9
+class Bear:
+    def eats(self):
+        return 'berries'
+
+class Rabbit:
+    def eats(self):
+        return 'clover'
+
+class Octothorpe:
+    def eats(self):
+        return 'campers'
+
+b = Bear()
+r = Rabbit()
+o = Octothorpe()
+
+print(b.eats())
+print(r.eats())
+print(o.eats())
 
 
-el_dict = {'name': 'Hydrogen', 'symbol': 'H', 'number': 1}
-
-class Element:
-    def __init__(self, name, symbol, number):
-        self.__name = name
-        self.__symbol = symbol
-        self.__number = number
-        @property
-        def names(self):
-            return (self.__name, self.__symbol, self.__number)
-        @name.setter
-        def name2(self, name):
-            self.__name = name
-            # self.__symbol = symbol
-            # self.__number = number
-
-    # def __str__(self):
-    #     return(f'{self.name},안녕 {self.symbol}, {self.number}')
 
 
-hydrogen = Element("Hydden","H",1)
-hydrogen.names()
-print(hydrogen)
+
 
 
 
